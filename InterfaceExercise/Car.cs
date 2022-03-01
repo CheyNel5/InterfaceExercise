026bdbe652e311b14842f7b4ef55169c0095afe0
CheyNel5 <cnelson9005@gmail.com>
2022-03-01 10:29:35 -0500

    initial commit

---
+++ InterfaceExercise/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace InterfaceExercise
+{
+    public class Car : IVehicle , ICompany
+    {
+        public Car()
+        {
+        }
+
+        public double EngineSize { get; set; } = 4.6;
+        public string Model { get; set; } = "Mustang";
+        public string Make { get; set; } = "Ford";
+        public int SeatCount { get; set; } = 4;
+        public string CompanyName { get; set; } = "Ford";
+        public string Motto { get; set; } = "Built Ford Tough";
+        public bool HasChangedGears { get; set; } = true;
+
+        public void Drive()
+        {
+            Console.WriteLine($"{GetType().Name} now driving forward. . .");
+        }
+
+        public void Reverse()
+        {
+            if(HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing. . .");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't reverse until we change gears");
+            }
+        }
+
+        public void Park()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing. . .");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine("Can't park until we change gears");
+            }
+        }
+
+        public void ChangeGears(bool isChanged)
+        {
+            HasChangedGears = isChanged;
+        }
+    }
+}
